0ae4586d715c28e3df8d10de2b45f9e4e17e7766
karen-gup <ana.kgp25@gmail.com>
2021-07-10 17:16:11 -0500

    funciones testeadas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import validator from './validator.js';
+const btn = document.getElementById("validar");
+btn.addEventListener("click", function() {
+    const card = document.getElementById("numberCard").value;
+    const valid = validator.isValid(card)
+    const hide = validator.maskify(card)
+    document.getElementById("p2").innerHTML = hide;
+   
 
-console.log(validator);
+    if (card===""){
+        document.getElementById("retrn").style.display = "block";
+        document.getElementById("p1").style.display = "block";
+        document.getElementById("p1").innerHTML = "No ingresaste ningun numero, vuelva a intentar";
+        document.getElementById("div1").style.display = "none";
+        document.getElementById("p2").style.display = "block";
+    } else if (valid === true) {
+        document.getElementById("retrn").style.display = "block";
+        document.getElementById("p1").style.display = "block";
+        document.getElementById("p1").innerHTML = "Tu tarjeta es valida";
+        document.getElementById("div1").style.display = "none";
+        document.getElementById("p2").style.display = "block";
+    }else {
+        document.getElementById("retrn").style.display = "block";
+        document.getElementById("p1").style.display = "block";
+        document.getElementById("p1").innerHTML = "Tu tarjeta es invalida";
+        document.getElementById("div1").style.display = "none";
+        document.getElementById("p2").style.display = "block";
+    }
+})
+
+const regresar = document.getElementById("retrn");
+regresar.addEventListener("click", function() {
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("retrn").style.display = "none";
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("p2").style.display = "none";
+    document.getElementById("numberCard").value = "";
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
+
 const validator = {
-  // ...
-};
+
+  isValid: function(creditCard){
+    const arreglo= Array.from(creditCard).reverse().map(Number);
+    let luhn= 0
+    for (let i=0; i<arreglo.length; i++){
+      if(i%2===1){
+        let duplicar= arreglo[i]*2;
+        if(duplicar>=10){
+          let producto= duplicar -9
+          luhn += producto;
+      }else{
+        luhn += duplicar;
+      }
+      }else{
+        luhn += arreglo[i];
+      }
+    }
+    let numFinal= luhn%10;
+    if(numFinal===0){
+      return true;
+    }else{
+      return false;
+    }
+  },
+  maskify: function(Ocultar){
+    let longArray= Ocultar.lengt
+    let miArray= Array.from(Ocultar)
+    if (longArray<=4){
+      return Ocultar
+    }else{
+      for(let i=0; i<Ocultar.length -4; i++){
+        miArray[i]='#'
+      }
+      let masknumber= miArray.join("");
+      return masknumber;
+    }
+   }
+}
+
 
 export default validator;
