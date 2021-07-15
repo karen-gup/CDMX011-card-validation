df3691e0b18f5ad74dbcfde433c31559d00a45e1
karen-gup <ana.kgp25@gmail.com>
2021-07-15 02:38:25 -0500

    Para ProyectFeedback

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
-# Sobre card validator.
-Opte por un diseño simple 
+# Tituto del proyecto CARD VALIDATOR.
+Card validator es un sitio web que ayuda a saber si una tarjeta de crédito/debito es válida o no de forma segura y práctica.
+# imagen final del proyecto 
+![img1](src/img/img1.png)
+![img2](src/img/img2.png)
+![img3](src/img/img2.png)
+
+# Investigación UX:
+Este verificador está pensado sobre todo en los usuarios web que utilizan tarjetas digitales y no estén seguros de la validez de sus tarjetas digitales o a quienes tengan curiosidad de si es válida o no alguna tarjeta que acaben de tramitar. 
+# Primer prototipo en papel:
+![img5](src/img/img5.jpeg)
+Resumen de feedback: Me recomendaron no juntar toda mi pagina en el centro, así que la distribui de forma horizontal, tambien que al cambiar de pagina para solo dejar el resultado de la validez de la tarjeta estaba dejando mucho espacio.
+![img4](src/img/img4.png)
 
 
-# Objetivos que prioritizaste este proyecto
- * DOM
- Uso de selectores del DOM.
- Manejo de eventos del DOM.
- Manipulación dinámica del DOM.
- * html y css
- Uso de HTML semántico.
- Uso de selectores de CSS.
- * JavaScript
- Manipulación de strings.
- Uso de condicionales (if-else | switch | operador ternario)
- Uso de bucles (for | for..in | for..of | while)
- Uso de funciones (parámetros | argumentos | valor de retorno)
- Declaración correcta de variables (const & let)
- * Estructura del código y guía de estilo
- Organizar y dividir el código en módulos (Modularización)
- Uso de identificadores descriptivos (Nomenclatura | Semántica)
- Uso de linter (ESLINT)
- * Testing
- * UX 
- Diseñar la aplicación pensando y entendiendo al usuario.
- Crear prototipos para obtener feedback e iterar.
- Aplicar los principios de diseño visual 
--- src/validator.js
+++ src/validator.js
@@ -1,43 +1,40 @@
 
 const validator = {
-
-  isValid: function(creditCard){
-    const arreglo= Array.from(creditCard).reverse().map(Number);
-    let luhn= 0
-    for (let i=0; i<arreglo.length; i++){
-      if(i%2===1){
-        let duplicar= arreglo[i]*2;
-        if(duplicar>=10){
-          let producto= duplicar -9
-          luhn += producto;
-      }else{
-        luhn += duplicar;
+  isValid: function (creditCard) {
+    const arreglo = Array.from(creditCard).reverse().map(Number); //Pasa el string como array, lo invierte y lo declara como number //
+    let sum = 0; //Variable reasignable para las operaciones
+    for (let i = 0; i < arreglo.length; i++) {
+      if (i%2!== 0) { //Si la posicion es impar...
+        let double = arreglo[i] * 2; //Toma el valor y lo duplica
+        if (double >= 10) { // Si el producto tiene dos digitos
+          let producto = double - 9 //Se le resta 9
+           sum += producto; //y se suman los digitos 
+        } else {
+          sum += double; //Si no tienen dos digitos, solo se duplican y se suman
+        }
+      } else {
+         sum += arreglo[i];//Si la posicion no es impar, se toman los valores del arreglo y se suman a la var
+        }
+     }
+    let numFinal = sum % 10; //El residuo de la suma total entre 10
+    if (numFinal === 0) { //Si el residuo es 0 el algoritmo es valido
+        return true;
+    } else {
+        return false;
       }
-      }else{
-        luhn += arreglo[i];
+    },
+  maskify: function (mask) {
+    let miArray = Array.from(mask) //definir arreglo
+    let longArray = mask.lengt; //longitud del arreglo
+    if (longArray <= 4) { //Si la longitud del arreglo es igual o menor a 4
+      return miArray //devuelve el arreglo 
+    } else {  
+      for (let i = 0; i < miArray.length - 4; i++) {
+        miArray[i] = '#' //Si no, devolvera el array enmascarado con #, excepto los ultimos cuatro digitos
+        }
+      let masknumber = miArray.join(""); //Con .joing hago que mi arreglo no este dividido o separado al mostrarlo
+        return masknumber;
       }
-    }
-    let numFinal= luhn%10;
-    if(numFinal===0){
-      return true;
-    }else{
-      return false;
-    }
-  },
-  maskify: function(Ocultar){
-    let longArray= Ocultar.lengt
-    let miArray= Array.from(Ocultar)
-    if (longArray<=4){
-      return Ocultar
-    }else{
-      for(let i=0; i<Ocultar.length -4; i++){
-        miArray[i]='#'
-      }
-      let masknumber= miArray.join("");
-      return masknumber;
-    }
-   }
-}
-
-
-export default validator;
+  }
+  }
+  export default validator;
